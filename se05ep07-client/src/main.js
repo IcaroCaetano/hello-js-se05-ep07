192447d1e9e86b7ec750d93c2cd7de1a948dd528
unknown <Fanor@FDN-WS-023537.acad.dvuadmin.net>
2018-02-03 12:00:10 -0200

    Lesson 07 Part 2

--- se05ep07-client/src/main.js
+++ se05ep07-client/src/main.js
@@ -1,8 +1,19 @@
-// main.js
-const foo = require("./mymodule1")
-const bar = require("./anothermodule").bar
+// // main.js
+// const foo = require("./mymodule1")
+// const bar = require("./anothermodule").bar
 
-foo(2)
-console.log(bar(3))
+// foo(2)
+// console.log(bar(3))
 
-alert("hello from browserify")// alert só é válido no browser
+// alert("hello from browserify")// alert só é válido no browser
+const Vue = require("vue")
+const VueMaterial = require("vue-material")
+
+Vue.use(VueMaterial)
+
+Vue.component("hello-vue", require("./hello.vue"))
+
+new Vue({
+  el: "#mountpoint",
+  render: r => r("hello-vue")
+})
