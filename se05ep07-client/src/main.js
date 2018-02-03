31d3e0d9bdac2430e49a17c0cbb15339948e23a1
unknown <Fanor@FDN-WS-023537.acad.dvuadmin.net>
2018-02-03 11:09:09 -0200

    Lesson 07

---
+++ se05ep07-client/src/main.js
@@ -0,0 +1,8 @@
+// main.js
+const foo = require("./mymodule1")
+const bar = require("./anothermodule").bar
+
+foo(2)
+console.log(bar(3))
+
+alert("hello from browserify")// alert só é válido no browser
---
+++ src/main.js
@@ -0,0 +1,2 @@
+const foo = require("./mymodule1")
+const bar = require("./anothermodule").bar
